b58fdcd517b47a5460e39b1bd0a1c2a0cb2555fe
evanrayburn2 <evanrayburn2@gmail.com>
2026-04-27 22:45:58 -0400

    can input name hooray

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,30 @@ export default function App() {
       setSubmitted(true)
   }
   return(
-      <div className="flex flex-col items-center justify-center h-screen bg-zinc-400">
+      <div className="flex flex-col items-center justify-center h-screen bg-mist-900">
+        {submitted && (
+            <h1 className = "text-3x1 mb-10 text-mauve-100">
+              You are {name}
+
+            </h1>
+        )}
+        {!submitted &&(
+            <div className="flex flex-col items-center gap-4">
+            <h1 className = "text-3x1 mb-10 text-mauve-100">
+              Input name
+            </h1>
+              <input
+                  className="p-3 text-mauve-100 w-64 outline-1"
+                  placeholder="Enter your name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)} //e is just on event updates everytime a letter is typed
+              />
+              <button className={"p-3 text-mauve-100 w-64 outline-1"} onClick={handleSubmit}>
+                Enter
+            </button>
+            </div>
+        )}
+
 
       </div>
   )
